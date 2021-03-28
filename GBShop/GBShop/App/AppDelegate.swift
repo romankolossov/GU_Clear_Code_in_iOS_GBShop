966bbfd155c7ac1a7e057a81998e2c8ffc2bede4
romankolossov <rkolossov@gmail.com>
2021-03-28 10:29:20 +0300

    hw10 firebase crashlytics task done

--- GBShop/GBShop/App/AppDelegate.swift
+++ GBShop/GBShop/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -43,6 +44,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // Use the Firebase library to configure APIs.
+          FirebaseApp.configure()
         return true
     }
 
