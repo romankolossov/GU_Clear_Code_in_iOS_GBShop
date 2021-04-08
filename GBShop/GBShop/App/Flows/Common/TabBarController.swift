f3801f9d5a24d910b62dcc64dc560b5459efb177
romankolossov <rkolossov@gmail.com>
2021-04-08 15:23:15 +0300

    refactor some code look changes done

--- GBShop/GBShop/App/Flows/Common/TabBarController.swift
+++ GBShop/GBShop/App/Flows/Common/TabBarController.swift
@@ -15,8 +15,8 @@ class TabBarController: UITabBarController {
         super.init(nibName: nil, bundle: nil)
         viewControllers = createViewControllers()
 
-        tabBar.backgroundColor = UIColor.tabBarBackgroundColor
-        tabBar.tintColor = UIColor.tabBarTintColor
+        tabBar.backgroundColor = .tabBarBackgroundColor
+        tabBar.tintColor = .tabBarTintColor
     }
     @available(*, unavailable) required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
