2d535314934741d5b7ef33420af5546e1c94174e
romankolossov <rkolossov@gmail.com>
2021-04-08 19:06:09 +0300

    more refactoring done 4

--- GBShop/GBShop/App/SceneDelegate.swift
+++ GBShop/GBShop/App/SceneDelegate.swift
@@ -12,16 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let windowFrame = UIScreen.main.bounds
+        guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        let windowFrame = UIScreen.main.bounds
         self.window = UIWindow(frame: windowFrame)
+
         self.window?.makeKeyAndVisible()
+        self.window?.windowScene = windowScene
 
         let tabBarController = TabBarController()
         self.window?.rootViewController = tabBarController
-
-        let windowScene = scene as? UIWindowScene
-        self.window?.windowScene = windowScene
 
         // MARK: Set dark InterfaceStyle
 
@@ -30,7 +30,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
